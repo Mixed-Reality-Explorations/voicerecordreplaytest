90348c33f6dd268b0dbcf2ba449e31f3fbff361c
molecule <molecule@berkeley.edu>
2019-03-17 15:50:20 -0700

    jniLibs shows up w these gradle changes. Still need to port more things over

--- app/src/main/java/io/github/molecule/voice_record_replay_test/ARActivity.java
+++
@@ -1,6 +0,0 @@
-package io.github.molecule.voice_record_replay_test;
-
-import android.support.v7.app.AppCompatActivity;
-
-public class ARActivity extends AppCompatActivity {
-}
---
+++ app/src/main/java/io/github/molecule/voice_record_replay_test/VuforiaSampleCode/SampleAppMenuInterface.java
@@ -0,0 +1,11 @@
+package io.github.molecule.voice_record_replay_test.VuforiaSampleCode;
+
+/**
+ * This interface is implemented in the activities to handle any desired behaviour
+ * associated with changing a menu option
+ */
+public interface SampleAppMenuInterface
+{
+    boolean menuProcess(int command);
+}
+
